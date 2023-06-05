aaabcfbb934cfa683d51b93139530c6be888e89c
Sébastien Santoro <dereckson@espace-win.org>
2023-06-05 22:36:48 +0000

    Adhere to code conventions
    Summary:
    As this repository is a resource to help with code style,
    files inside should be correctly formatted:
    
      - use 4 spaces for XML
      - apply them to a hird-party pre-commit hook
    
    "We eat our own dog food."
    
    Test Plan: `ec` (editorconfig-checker)
    
    Reviewers: dereckson
    
    Reviewed By: dereckson
    
    Differential Revision: https://devcentral.nasqueron.org/D3175

--- pre-commit-hooks/shellcheck.sh
+++ pre-commit-hooks/shellcheck.sh
@@ -19,43 +19,43 @@ enable_list=""
 severity="style"
 
 parse_arguments() {
-	while (($# > 0)); do
-		# Grab param and value splitting on " " or "=" with parameter expansion
-		local PARAMETER="${1%[ =]*}"
-		local VALUE="${1#*[ =]}"
-		if [[ "$PARAMETER" == "$VALUE" ]]; then VALUE="$2"; fi
-		shift
-		case "$PARAMETER" in
-		--enable)
-			enable_list="$enable_list $VALUE"
-			;;
-		--severity)
-			severity=$VALUE
-			;;
-		-*)
-			echo "Error: Unknown option: $PARAMETER" >&2
-			exit 1
-			;;
-		*)
-			files="$files $PARAMETER"
-			;;
-		esac
-	done
-	enable_list="${enable_list## }" # remove preceeding space
+    while (($# > 0)); do
+        # Grab param and value splitting on " " or "=" with parameter expansion
+        local PARAMETER="${1%[ =]*}"
+        local VALUE="${1#*[ =]}"
+        if [[ "$PARAMETER" == "$VALUE" ]]; then VALUE="$2"; fi
+        shift
+        case "$PARAMETER" in
+        --enable)
+            enable_list="$enable_list $VALUE"
+            ;;
+        --severity)
+            severity=$VALUE
+            ;;
+        -*)
+            echo "Error: Unknown option: $PARAMETER" >&2
+            exit 1
+            ;;
+        *)
+            files="$files $PARAMETER"
+            ;;
+        esac
+    done
+    enable_list="${enable_list## }" # remove preceeding space
 }
 
 parse_arguments "$@"
 
 for FILE in $files; do
-	SHEBANG_REGEX='^#!\(/\|/.*/\|/.* \)\(\(ba\|da\|k\|a\)*sh\|bats\)$'
-	if (head -1 "$FILE" | grep "$SHEBANG_REGEX" >/dev/null); then
-		if ! shellcheck ${enable_list:+ --enable="$enable_list"} --severity="$severity" "$FILE"; then
-			exit_status=1
-		fi
-	elif [[ "$FILE" =~ .+\.(sh|bash|dash|ksh|ash|bats)$ ]]; then
-		echo "$FILE: missing shebang"
-		exit_status=1
-	fi
+    SHEBANG_REGEX='^#!\(/\|/.*/\|/.* \)\(\(ba\|da\|k\|a\)*sh\|bats\)$'
+    if (head -1 "$FILE" | grep "$SHEBANG_REGEX" >/dev/null); then
+        if ! shellcheck ${enable_list:+ --enable="$enable_list"} --severity="$severity" "$FILE"; then
+            exit_status=1
+        fi
+    elif [[ "$FILE" =~ .+\.(sh|bash|dash|ksh|ash|bats)$ ]]; then
+        echo "$FILE: missing shebang"
+        exit_status=1
+    fi
 done
 
 exit $exit_status
